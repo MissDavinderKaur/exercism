02eaa09e84cbcb1e453accce82465cb43f19aa00
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-04-10 09:16:52 +0100

    Bob exercism is complete

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,27 +1,13 @@
 var Bob = function() {};
 
-Bob.prototype.hey = function(string) {
-  if (string === ''){
-    return 'Fine. Be that way!';
-  } else if (this.checkAllUpperCase(string)) {
-    return 'Whoa, chill out!';
-  } else if (string[string.length - 1] === '?') {
-    return 'Sure.';
-  } else {
-    return 'Whatever.';
-  }
+Bob.prototype.hey = function(input) {
+	if ( input.toUpperCase() === input && /[A-Z]/.test(input))
+		return 'Whoa, chill out!';
+	if ( /\?\s*$/.test(input) )
+		return 'Sure.';
+	if ( /^\s*$/.test(input))
+		return 'Fine. Be that way!';
+	return 'Whatever.';
 };
-
-Bob.prototype.checkAllUpperCase = function(string) {
-  const arr = string.split('');
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i].toUpperCase()) {
-      return false;
-    }
-  }
-  return true;
-};
-
 
 module.exports = Bob;
